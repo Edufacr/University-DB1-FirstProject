03306513b60d50ae0e5383b66125d2a97a0de33d
JorgeGuty <49891177+JorgeGuty@users.noreply.github.com>
2020-05-28 23:44:58 -0600

    SP_insertPropertyOwner
    SP_insertPropertyOwner alter and creation versions added.
    Minor changes to insertOwner scripts

---
+++ University-DB1-FirstProject/CreationSQLs/SP_insertPropertyOwner_Creation.sql
@@ -0,0 +1,33 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Jorge Gutiérrez Cordero
+-- Create date: 2020/5/28
+-- Description:	Insert a property owner to DB1P_PropertyOwners
+-- =============================================
+create procedure dbo.SP_insertPropertyOwner @pProperty_Id int, @pOwner_Id int
+as
+begin
+	begin try
+		insert into dbo.DB1P_PropertyOwners (Property_Id, Owner_Id, Active)
+		values (@pProperty_Id, @pOwner_Id, 1);
+		return 1
+	end try
+	begin catch
+		return @@Error * -1
+	end catch		
+end
+
