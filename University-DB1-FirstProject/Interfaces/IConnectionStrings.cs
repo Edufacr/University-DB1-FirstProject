5e3f14b35e1ce503afc43594f95feba390711531
JorgeGuty <49891177+JorgeGuty@users.noreply.github.com>
2020-06-03 00:00:15 -0600

    Commit

---
+++ University-DB1-FirstProject/Interfaces/IConnectionStrings.cs
@@ -0,0 +1,14 @@
+﻿namespace University_DB1_FirstProject.Interfaces
+{
+    public interface IConnectionStrings
+    {
+        //To place here the connection string to be used.
+        public static string CONNECTIONSTRING = JORGECONNECTIONSTR;
+
+        public static string JORGECONNECTIONSTR =
+            @"Data Source = LAPTOP-5URQT67L\SQLEXPRESS; Initial Catalog = DB1-FirstProject; Integrated Security = True";
+        public static string EDUARDOCONNECTIONSTR = "";
+    }
+
+
+}
