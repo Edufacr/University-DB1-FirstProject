7cc0eb43592fa22dff47be11994ba39bcc5aed39
Eduardo <edumadrim@gmail.com>
2020-06-07 15:10:37 -0600

    fix user view & added controllers logic

--- University-DB1-FirstProject/Controllers/Results.cs
+++ University-DB1-FirstProject/Controllers/Results.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using University_DB1_FirstProject.Models;
 
@@ -10,10 +11,10 @@ namespace University_DB1_FirstProject.Controllers
     {
         private readonly PropertyController _propertyController = PropertyController.getInstance();
         private readonly UserController _userController = UserController.getInstance();
+        private readonly OwnerController _ownerController = OwnerController.getInstance();
         // GET
         public IActionResult PropertiesUsers(int pPropertyNumber)
         {
-            Console.WriteLine(pPropertyNumber);
             ViewData["Id"] = pPropertyNumber;
             PropertyDisplayModel property = new PropertyDisplayModel {PropertyNumber = pPropertyNumber};
             List<UserDisplayModel> list = _userController.ExecuteGetUsersOfProperty(property);
@@ -21,11 +22,14 @@ namespace University_DB1_FirstProject.Controllers
         }
         public IActionResult PropertyInfo(int pPropertyNumber)
         {
-            return View();
+            ViewData["Id"] = pPropertyNumber;
+            PropertyDisplayModel property = new PropertyDisplayModel(){PropertyNumber = pPropertyNumber};
+            List<PropertyDisplayModel> list = _propertyController.ExecuteGetPropertyInfoByPropertyNumber(property);
+            return View(list.First());
         }
         public IActionResult AllProperties()
         {
-            List<PropertyDisplayModel> list = new List<PropertyDisplayModel>();
+            List<PropertyDisplayModel> list = _propertyController.ExecuteGetActiveProperties();
             return View(list);
         }
         public IActionResult InsertProperty(int pPropertyNumber,string pAddress, float pValue)
@@ -39,12 +43,21 @@ namespace University_DB1_FirstProject.Controllers
         }
         public IActionResult UpdateProperty(int pPropertyNumber,string pAddress, float pValue, int pNewPropertyNumber)
         {
+            PropertyRegisterModel newValues = new PropertyRegisterModel()
+            {
+                PropertyNumber = pNewPropertyNumber, Address = pAddress, Value = pValue
+            };
+            PropertyDisplayModel original = new PropertyDisplayModel()
+            {
+                PropertyNumber = pPropertyNumber
+            };
+            _propertyController.ExecuteUpdateProperty(original, newValues);
             return View("Accomplished");
         }
         public IActionResult DeleteProperty(int pPropertyNumber)
         {
             PropertyDisplayModel property = new PropertyDisplayModel {PropertyNumber = pPropertyNumber};
-            Console.WriteLine(pPropertyNumber);
+            _propertyController.ExecuteDeleteProperty(property);
             return View("Accomplished");
         }
         
@@ -52,59 +65,75 @@ namespace University_DB1_FirstProject.Controllers
         public IActionResult OwnerProperties(string pDocValue)
         {
             ViewData["Id"] = pDocValue;
-            return View();
+            return View(_propertyController.ExecuteGetPropertiesOfOwner(new OwnerDisplayModel{DocValue = pDocValue}));
         }
         public IActionResult AllOwners()
         {
-            return View();
+            return View(_ownerController.ExcecuteGetActiveOwners());
         }
 
         public IActionResult OwnerInfo(string pDocValue)
         {
+            ViewData["Id"] = pDocValue;
+            _ownerController.ExcecuteGetOwnersByDocValue(new OwnerDisplayModel{DocValue = pDocValue});
             return View();
         }
         public IActionResult OwnerInfoByName(string pName)
         {
+            ViewData["Id"] = pName;
             return View("OwnerInfo");
         }
         public IActionResult InsertOwner(string pDocValue,int pDocTypeId, string pName)
         {
+            _ownerController.ExecuteInsertOwner(new OwnerRegisterModel
+                {DocValue = pDocValue, DocTypeId = pDocTypeId, Name = pName});
             return View("Accomplished");
         }
         public IActionResult UpdateOwner(string pDocValue,int pDocTypeId, string pName, string pNewDocValue)
         {
+            _ownerController.ExecuteUpdateOwner(new OwnerDisplayModel {DocValue = pDocValue},
+                new OwnerRegisterModel {DocValue = pNewDocValue, DocTypeId = pDocTypeId, Name = pName});
             return View("Accomplished");
         }
         public IActionResult DeleteOwner(string pDocValue)
         {
+            _ownerController.ExecuteDeleteOwner(new OwnerDisplayModel {DocValue = pDocValue});
             return View("Accomplished");
         }
         
         //Users  ARREGLAR PARAMETROS
-        public IActionResult UserProperties(string pDocValue)
+        public IActionResult UserProperties(string pName)
         {
-            ViewData["Id"] = pDocValue;
-            return View();
+            ViewData["Id"] = pName;
+            return View(_propertyController.ExecuteGetPropertiesOfUser(new UserDisplayModel{Name = pName}));
         }
         public IActionResult AllUsers()
         {
-            return View();
+            return View(_userController.ExecuteGetActiveUsers());
         }
 
-        public IActionResult UserInfo(string pDocValue)
+        public IActionResult UserInfo(string pName)
         {
-            return View();
+            ViewData["Id"] = pName;
+            return View(_userController.ExecuteGetPassword(new UserDisplayModel{Name = pName}));
         }
-        public IActionResult InsertUser(string pDocValue,int pDocTypeId, string pName)
+        public IActionResult InsertUser(string pName,string pPassword,int pIsAdmin)
         {
+            _userController.ExecuteInsertUser(new UserRegisterModel
+                {Name = pName, Password = pPassword, UserType = pIsAdmin});
             return View("Accomplished");
         }
-        public IActionResult UpdateUser(string pDocValue,int pDocTypeId, string pName, string pNewDocValue)
+        public IActionResult UpdateUser(string pName,string pPassword,int pIsAdmin, string pNewName)
         {
+            UserDisplayModel original = new UserDisplayModel {Name = pName};
+            _userController.ExecuteUpdateUser(original,
+                new UserRegisterModel
+                    {Name = pNewName, UserType = pIsAdmin, Password = pPassword});
             return View("Accomplished");
         }
-        public IActionResult DeleteUser(string pDocValue)
+        public IActionResult DeleteUser(string pName)
         {
+            _userController.ExecuteDeleteUser(new UserDisplayModel {Name = pName});
             return View("Accomplished");
         }
     }
